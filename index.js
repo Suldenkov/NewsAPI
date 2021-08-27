a384453294e8023a4ae0964e63c7c894f389852b
Vladimir <suldenkovv@mail.ru>
2021-08-27 23:44:10 +0300

    add filter scrip

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const btn = document.querySelector('#btn');
-const list = document.querySelector('.container');
+const   btn = document.querySelector('#btn');
+        list = document.querySelector('.container');
+        category = document.querySelector('.category');
+        language = document.querySelector('.language');
+        country = document.querySelector('.country');
+        apiKey = '4dd10ac3b26049f8b53ffbd4d98c419f';
+        URL = `https://newsapi.org/v2/top-headlines`;
+
+let     showCategory = 'business';
+        showCountry = 'us';
 
 const getCard = ({title, url, description, urlToImage}) => {
     return `
     <div class="card">
         <div class="card__fragment">
-            <img class="card__image" src="${urlToImage}" alt="">
-            <span class="card__title">${title}</span>
+            <img class="card__image" src="${urlToImage || './img/imgNotFound.jpg'}" alt="">
+            <span class="card__title">${title || ''}</span>
         </div>
         <div class="card__content">
-            <p>${description}</p>
+            <p>${description || ''}</p>
         </div>
         <div class="card__action">
             <a href="${url}"></a>
@@ -20,24 +28,34 @@ const getCard = ({title, url, description, urlToImage}) => {
 const renderCards = (news) =>{
     let fragment = '';
 
-    news.forEach(element => {  
+    news.forEach(element => {
         fragment += getCard(element);
     });
-
     list.innerHTML = fragment;
 }
 
 async function getNews()
 {
     try{
-        let data = await axios.get('https://newsapi.org/v2/everything?q=Apple&from=2021-08-26&sortBy=popularity&apiKey=4dd10ac3b26049f8b53ffbd4d98c419f');
+        let data = await axios.get(URL + `?country=${showCountry}&category=${showCategory}&apiKey=${apiKey}`);
         renderCards(data.data.articles);
+        console.log('ok');
     }
     catch(error){
         console.log(error.status);
     }
 }
 
-btn.addEventListener('click', () => {
+category.addEventListener('change', (e) => {
+    showCategory = e.target.value;
+    window.scrollTo(0, 0)
     getNews();
 })
+
+country.addEventListener('change', (e) => {
+    showCountry = e.target.value;
+    window.scrollTo(0, 0);
+    getNews();
+})
+
+getNews();
